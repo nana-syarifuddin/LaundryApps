fde8e933c3689f7f7a1b9f10c033cdddf071e685
nana-syarifuddin <nana.informatika@students.amikom.ac.id>
2023-11-24 10:22:45 +0700

    Inisialisasi Project

---
+++ LaundryApps/LaundryApps/Controller/DashboardController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SQLite;
+
+namespace LaundryApps.Controller
+{
+    class DashboardController
+    {
+        Model.Dashboard model;
+        View.Dashboard view;
+        Model.KoneksiDB db;
+
+        public DashboardController(View.Dashboard view)
+        {
+            model = new Model.Dashboard();
+            db = new Model.KoneksiDB();
+            this.view = view;
+        }
+        public void InisialisasiView()
+        {
+            view.lvwTransaksi.View = System.Windows.Forms.View.Details;
+            view.lvwTransaksi.FullRowSelect = true;
+            view.lvwTransaksi.GridLines = true;
+
+            view.lvwTransaksi.Columns.Add("No.", 30, HorizontalAlignment.Center);
+            view.lvwTransaksi.Columns.Add("ID Transaksi", 80, HorizontalAlignment.Center);
+            view.lvwTransaksi.Columns.Add("Nama", 80, HorizontalAlignment.Left);
+            view.lvwTransaksi.Columns.Add("Tanggal Order", 80, HorizontalAlignment.Left);
+            view.lvwTransaksi.Columns.Add("Total Harga", 80, HorizontalAlignment.Left);
+            view.lvwTransaksi.Columns.Add("Status", 70, HorizontalAlignment.Left);
+            view.lvwTransaksi.Columns.Add("Metode Bayar", 70, HorizontalAlignment.Left);
+            view.lvwTransaksi.Columns.Add("Catatan", 80, HorizontalAlignment.Left);
+        }
+
+        public void tampilkanData()
+        {
+            bool result = model.tampilkanData();
+
+            if (result)
+            {
+                view.lvwTransaksi.Items.Clear();
+
+                db = new Model.KoneksiDB();
+
+                SQLiteConnection conn = Model.KoneksiDB.GetOpenConnection();
+
+                conn.Open();
+
+                string sql = "SELECT transaksi.id_transaksi, userdata.nama, transaksi.tanggal_order, transaksi.total_harga, transaksi.status, transaksi.metode_bayar, transaksi.catatan FROM transaksi, userdata WHERE transaksi.id_user = userdata.id_user";
+
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+
+                SQLiteDataReader dtr = cmd.ExecuteReader();
+
+                while (dtr.Read())
+                {
+                    var noUrut = view.lvwTransaksi.Items.Count + 1;
+                    var item = new ListViewItem(noUrut.ToString());
+                    item.SubItems.Add(dtr["id_transaksi"].ToString());
+                    item.SubItems.Add(dtr["nama"].ToString());
+                    item.SubItems.Add(dtr["tanggal_order"].ToString());
+                    item.SubItems.Add(dtr["total_harga"].ToString());
+                    item.SubItems.Add(dtr["status"].ToString());
+                    item.SubItems.Add(dtr["metode_bayar"].ToString());
+                    item.SubItems.Add(dtr["catatan"].ToString());
+                    view.lvwTransaksi.Items.Add(item);
+                }
+                dtr.Dispose();
+                cmd.Dispose();
+                conn.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Kesalahan Pada Tabel Transaksi", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public void totalPendapatan()
+        {
+            bool result = model.totalPendapatan();
+
+            if (result)
+            {
+                db = new Model.KoneksiDB();
+
+                SQLiteConnection conn = Model.KoneksiDB.GetOpenConnection();
+
+                conn.Open();
+
+                string sql = "SELECT sum(total_harga) FROM transaksi";
+
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+
+                object hasil = cmd.ExecuteScalar();
+                view.lblTotalPendapatan.Text = Convert.ToString(hasil);
+
+                cmd.Dispose();
+                conn.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Kesalahan Pada Tabel Transaksi", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public void laporan()
+        {
+            bool result = model.laporan();
+
+            if (result)
+            {
+                db = new Model.KoneksiDB();
+
+                SQLiteConnection conn = Model.KoneksiDB.GetOpenConnection();
+
+                conn.Open();
+
+                string sql = "SELECT COUNT(status) FROM transaksi";
+                string sql2 = "SELECT COUNT(status) FROM transaksi WHERE status='Diterima'";
+                string sql3 = "SELECT COUNT(status) FROM transaksi WHERE status='Proses'";
+                string sql4 = "SELECT COUNT(status) FROM transaksi WHERE status='Selesai'";
+
+                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                SQLiteCommand cmd2 = new SQLiteCommand(sql2, conn);
+                SQLiteCommand cmd3 = new SQLiteCommand(sql3, conn);
+                SQLiteCommand cmd4 = new SQLiteCommand(sql4, conn);
+
+                object hasil = cmd.ExecuteScalar();
+                object hasil2 = cmd2.ExecuteScalar();
+                object hasil3 = cmd3.ExecuteScalar();
+                object hasil4 = cmd4.ExecuteScalar();
+                view.lblTotalOrder.Text = Convert.ToString(hasil);
+                view.lblDiterima.Text = Convert.ToString(hasil2);
+                view.lblProses.Text = Convert.ToString(hasil3);
+                view.lblSelesai.Text = Convert.ToString(hasil4);
+
+                cmd.Dispose();
+                conn.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Kesalahan Pada Tabel Transaksi", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
---
+++ LaundryApps/LaundryApps/View/Dashboard.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace LaundryApps.View
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnLayanan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotalPendapatan = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblSelesai = new System.Windows.Forms.Label();
+            this.lblProses = new System.Windows.Forms.Label();
+            this.lblDiterima = new System.Windows.Forms.Label();
+            this.lblTotalOrder = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lvwTransaksi = new System.Windows.Forms.ListView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 53);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dashboard";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 100);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Pelanggan";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnLayanan
+            // 
+            this.btnLayanan.Location = new System.Drawing.Point(12, 129);
+            this.btnLayanan.Name = "btnLayanan";
+            this.btnLayanan.Size = new System.Drawing.Size(75, 23);
+            this.btnLayanan.TabIndex = 3;
+            this.btnLayanan.Text = "Layanan";
+            this.btnLayanan.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dashboard";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblTotalPendapatan);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(96, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(249, 123);
+            this.panel1.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(80, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Rp";
+            // 
+            // lblTotalPendapatan
+            // 
+            this.lblTotalPendapatan.AutoSize = true;
+            this.lblTotalPendapatan.Location = new System.Drawing.Point(111, 68);
+            this.lblTotalPendapatan.Name = "lblTotalPendapatan";
+            this.lblTotalPendapatan.Size = new System.Drawing.Size(25, 13);
+            this.lblTotalPendapatan.TabIndex = 14;
+            this.lblTotalPendapatan.Text = "000";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(249, 37);
+            this.panel3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Total Pendapatan";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblSelesai);
+            this.panel2.Controls.Add(this.lblProses);
+            this.panel2.Controls.Add(this.lblDiterima);
+            this.panel2.Controls.Add(this.lblTotalOrder);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(365, 41);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(249, 123);
+            this.panel2.TabIndex = 6;
+            // 
+            // lblSelesai
+            // 
+            this.lblSelesai.AutoSize = true;
+            this.lblSelesai.Location = new System.Drawing.Point(186, 106);
+            this.lblSelesai.Name = "lblSelesai";
+            this.lblSelesai.Size = new System.Drawing.Size(13, 13);
+            this.lblSelesai.TabIndex = 17;
+            this.lblSelesai.Text = "0";
+            // 
+            // lblProses
+            // 
+            this.lblProses.AutoSize = true;
+            this.lblProses.Location = new System.Drawing.Point(186, 86);
+            this.lblProses.Name = "lblProses";
+            this.lblProses.Size = new System.Drawing.Size(13, 13);
+            this.lblProses.TabIndex = 16;
+            this.lblProses.Text = "0";
+            // 
+            // lblDiterima
+            // 
+            this.lblDiterima.AutoSize = true;
+            this.lblDiterima.Location = new System.Drawing.Point(186, 63);
+            this.lblDiterima.Name = "lblDiterima";
+            this.lblDiterima.Size = new System.Drawing.Size(13, 13);
+            this.lblDiterima.TabIndex = 15;
+            this.lblDiterima.Text = "0";
+            // 
+            // lblTotalOrder
+            // 
+            this.lblTotalOrder.AutoSize = true;
+            this.lblTotalOrder.Location = new System.Drawing.Point(186, 40);
+            this.lblTotalOrder.Name = "lblTotalOrder";
+            this.lblTotalOrder.Size = new System.Drawing.Size(13, 13);
+            this.lblTotalOrder.TabIndex = 14;
+            this.lblTotalOrder.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Selesai";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Proses";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Diterima";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Total Order";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(365, 41);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(249, 37);
+            this.panel4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Laporan";
+            // 
+            // lvwTransaksi
+            // 
+            this.lvwTransaksi.HideSelection = false;
+            this.lvwTransaksi.Location = new System.Drawing.Point(96, 170);
+            this.lvwTransaksi.Name = "lvwTransaksi";
+            this.lvwTransaksi.Size = new System.Drawing.Size(603, 168);
+            this.lvwTransaksi.TabIndex = 8;
+            this.lvwTransaksi.UseCompatibleStateImageBehavior = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(624, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Lihat Semua";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 380);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lvwTransaksi);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLayanan);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnLayanan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.ListView lvwTransaksi;
+        public System.Windows.Forms.Label lblTotalPendapatan;
+        public System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Label lblTotalOrder;
+        public System.Windows.Forms.Label lblSelesai;
+        public System.Windows.Forms.Label lblProses;
+        public System.Windows.Forms.Label lblDiterima;
+        private System.Windows.Forms.Button button4;
+    }
+}
